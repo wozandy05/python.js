0356f75cd56b7af0280c1c469cf9712a69b95e99
Andy Woz <159766349+wozandy05@users.noreply.github.com>
2024-11-19 01:09:47 +0000

    Update range function

--- python.js
+++ python.js
@@ -19,6 +19,10 @@ function input(promptMessage) {
 }
 
 function range(start, end, step = 1) {
+  if (end === undefined) {
+    end = start;
+    start = 0;
+  }
   let arr = [];
   if (step > 0) {
     for (let i = start; i < end; i += step) {
